ee5b628d4a73ac102daf5c9a1506b0df31b0099f
Daniel <djbrior@gmail.com>
2019-01-01 12:59:05 -0500

    Add basic drawing capability and canvas reset and resizing

---
+++ script.js
@@ -0,0 +1,26 @@
+setup(parseInt(prompt("Canvas Dimensions")));
+
+function setup(num) {
+    var grid = document.getElementById("grid");
+    grid.style.gridTemplateColumns = "repeat(" + num.toString() + ",1fr)";
+    grid.style.gridTemplateRows = "repeat(" + num.toString() + ",1fr)";
+
+    for(var i=0;i<num*num;i++){
+        var split = 100/num;
+        var square = document.createElement("div");
+        square.style.width = split.toString() + "vw";
+        square.style.height = split.toString() + "vh";
+        square.addEventListener('mouseenter', (e) => {
+            e.target.classList.add("marked");
+        });
+        grid.appendChild(square);
+    }
+}
+
+function reset(){
+    var grid = document.getElementById("grid");
+    while(grid.lastChild){
+        grid.removeChild(grid.lastChild);
+    }
+    setup(parseInt(prompt("Canvas Dimensions")));     
+}
